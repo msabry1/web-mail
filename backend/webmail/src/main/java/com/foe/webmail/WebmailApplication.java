302f70790f3d6e32ea8355ea9f4bbe89bcec5be5
sabritos <msabrym2004@gmail.com>
2024-12-13 01:09:22 +0200

    define env variables in application.properties

--- backend/webmail/src/main/java/com/foe/webmail/WebmailApplication.java
+++ backend/webmail/src/main/java/com/foe/webmail/WebmailApplication.java
@@ -1,13 +1,16 @@
 package com.foe.webmail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebmailApplication {
-
+	@Value("${DB_PASSWORD}")
+	private static String dbPassword;
 	public static void main(String[] args) {
 		SpringApplication.run(WebmailApplication.class, args);
+    System.out.println(dbPassword);
 	}
 
 }
