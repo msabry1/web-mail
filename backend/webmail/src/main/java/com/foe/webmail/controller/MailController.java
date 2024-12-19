888ec1c25a89c41ec499f8e6d535ba73fd2dd0c6
sabritos <msabrym2004@gmail.com>
2024-12-19 10:48:50 +0200

    move compose from TestControlleer to MailController

--- backend/webmail/src/main/java/com/foe/webmail/controller/MailController.java
+++ backend/webmail/src/main/java/com/foe/webmail/controller/MailController.java
@@ -1,11 +1,13 @@
 package com.foe.webmail.controller;
 
 
+import com.foe.webmail.dto.EmailComposeDto;
 import com.foe.webmail.dto.FullMailDTO;
 
 import com.foe.webmail.dto.MailFilterDTO;
 import com.foe.webmail.dto.MailPreviewDTO;
 import com.foe.webmail.entity.UserPrinciple;
+import com.foe.webmail.service.EmailSenderService;
 import com.foe.webmail.service.mailService.MailServiceFacade;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +18,27 @@ import java.util.List;
 public class MailController {
 
     MailServiceFacade mailServiceFacade;
+    EmailSenderService emailSenderService;
 
-    public MailController(MailServiceFacade mailServiceFacade) {
+    public MailController(MailServiceFacade mailServiceFacade,
+                          EmailSenderService emailSenderService) {
         this.mailServiceFacade = mailServiceFacade;
+        this.emailSenderService = emailSenderService;
     }
 
-    @GetMapping("mails/sent")
+    @PostMapping("/compose")
+    public String compose(EmailComposeDto emailComposeDto, @AuthenticationPrincipal UserPrinciple userPrinciple) {
+        emailSenderService.sendEmail(emailComposeDto,userPrinciple.getUsername());
+        return "Sended";
+    }
+
+    @PostMapping("mails/sent")
     public List<MailPreviewDTO> sentMails(@RequestBody MailFilterDTO mailFilterDTO,
                                           @AuthenticationPrincipal UserPrinciple user) {
         return mailServiceFacade.getSentMails(mailFilterDTO, user);
     }
 
-    @GetMapping("mails/received")
+    @PostMapping("mails/received")
     public List<MailPreviewDTO> receivedMails(@RequestBody MailFilterDTO mailFilterDTO,
                                       @AuthenticationPrincipal UserPrinciple user) {
         return mailServiceFacade.getReceivedMails(mailFilterDTO, user);
